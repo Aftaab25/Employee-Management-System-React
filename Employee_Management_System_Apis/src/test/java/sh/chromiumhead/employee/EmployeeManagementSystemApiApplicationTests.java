0b295b86499fe6140e8f5da01b6fedbda63f7607
aftaab25 <aftaab2507@gmail.com>
2024-04-24 03:11:10 +0530

    Initial Commit EMS Backend

---
+++ Employee_Management_System_Apis/src/test/java/sh/chromiumhead/employee/EmployeeManagementSystemApiApplicationTests.java
@@ -0,0 +1,13 @@
+package sh.chromiumhead.employee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeManagementSystemApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Employee-Management-System-React
 Full Stack Employee Management App using React, Spring Boot, MySQL and TailWind CSS
+
+CREATE SCHEMA `employee_management_system` ;
+
